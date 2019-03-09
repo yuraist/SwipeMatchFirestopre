b77ae3c8fbf980570bdfe3d3c7d51bafbe7c04f3
Yuraist <yura.ist@icloud.com>
2019-03-09 15:27:00 +0300

    [card] refactor models to viewmodels

--- SwipeMatchFirestore/Controllers/HomeController.swift
+++ SwipeMatchFirestore/Controllers/HomeController.swift
@@ -14,9 +14,9 @@ class HomeController: UIViewController {
   let cardsDeckView = UIView()
   let buttonsStackView = HomeBottomControlsStackView()
   
-  let users = [
-    User(name: "Yura", age: 20, profession: "iOS Developer", imageName: "ava"),
-    User(name: "Polina", age: 21, profession: "UX/UI Designer", imageName: "polina")
+  let cardViewModels = [
+    User(name: "Yura", age: 20, profession: "iOS Developer", imageName: "ava").toCardViewModel(),
+    User(name: "Polina", age: 21, profession: "UX/UI Designer", imageName: "polina").toCardViewModel()
   ]
   
   override func viewDidLoad() {
@@ -44,15 +44,11 @@ class HomeController: UIViewController {
   }
   
   fileprivate func setupCards() {
-    users.forEach { (user) in
+    cardViewModels.forEach { (cardViewModel) in
       let cardView = CardView(frame: .zero)
-      cardView.imageView.image = UIImage(named: user.imageName)
-      
-      let attributedText = NSMutableAttributedString(string: user.name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
-      attributedText.append(NSAttributedString(string: "  \(user.age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
-      attributedText.append(NSAttributedString(string: "\n\(user.profession)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
-      
-      cardView.informationLabel.attributedText = attributedText
+      cardView.imageView.image = UIImage(named: cardViewModel.imageName)
+      cardView.informationLabel.attributedText = cardViewModel.attributedString
+      cardView.informationLabel.textAlignment = cardViewModel.textAlignment
       
       cardsDeckView.addSubview(cardView)
       cardView.fillSuperview()
---
+++ SwipeMatchFirestore/ViewModels/CardViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CardViewModel.swift
+//  SwipeMatchFirestore
+//
+//  Created by Юрий Истомин on 09/03/2019.
+//  Copyright © 2019 Treedo. All rights reserved.
+//
+
+import UIKit
+
+struct CardViewModel {
+  let imageName: String
+  let attributedString: NSAttributedString
+  let textAlignment: NSTextAlignment
+}
