9cd62d2339c347d0d4b333cd4f582ca1cd8eaed3
Yuraist <yura.ist@icloud.com>
2019-03-09 13:41:11 +0300

    [main] update card transformation

--- SwipeMatchFirestore/Views/CardView.swift
+++ SwipeMatchFirestore/Views/CardView.swift
@@ -16,6 +16,9 @@ class CardView: UIView {
     return imageView
   }()
   
+  // Configurations
+  fileprivate let threshold: CGFloat = 100
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     
@@ -34,7 +37,7 @@ class CardView: UIView {
     case .changed:
       handleChanged(gesture)
     case .ended:
-      handleEnded()
+      handleEnded(gesture)
     default:
       ()
     }
@@ -42,14 +45,27 @@ class CardView: UIView {
   
   fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
     let translation = gesture.translation(in: nil)
-    transform = CGAffineTransform(translationX: translation.x, y: translation.y)
+    let degrees: CGFloat = translation.x / 20
+    let angle = degrees * .pi / 180
+    
+    let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
+    transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
   }
   
-  fileprivate func handleEnded() {
+  fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+    let shouldDismissCard = gesture.translation(in: nil).x > threshold
+    
     UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-      self.transform = .identity
+      if shouldDismissCard {
+        self.frame = CGRect(x: 1000, y: 0, width: self.frame.width, height: self.frame.height)
+      } else {
+        self.transform = .identity
+      }
     }) { (_) in
-      
+      self.transform = .identity
+      self.frame = CGRect(x: 0, y: 0,
+                          width: self.superview!.frame.width,
+                          height: self.superview!.frame.height)
     }
   }
   
